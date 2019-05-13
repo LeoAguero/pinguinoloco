9173f5a688103535d692f67a26569198e08f28f5
LeoAguero <jhon66746@gmail.com>
2019-05-13 18:47:51 +0200

    hello

--- practicasdia7.py
+++ practicasdia7.py
@@ -1,3 +1,21 @@
 
 #oractica con control de version git
 print("hola que tal")
+
+
+################
+#comandos de git
+#################
+
+#git add.
+#dit commit -m "lo que quieras"
+#git push  (para empujar el codigo: del local al remoto)
+#git pull  (para estirar el codigo del remoto al local)
+
+###########
+#comandos para navegar en la terminal
+###########
+
+#ls o dir (para ver los archivos existentes)
+#cd nombre_de_archivo (para entrar en el archivo)
+#cd .. (para salir del archivo)
